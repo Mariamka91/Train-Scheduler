274fbca9b8c0f0e8a461d556cd56fe57b3c3cdd7
mariam kilasonia <mar.kilasonia@gmail.com>
2019-12-23 00:03:54 -0500

    create html and css

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+// Initialize Firebase
+var firebaseConfig = {
+    apiKey: "AIzaSyB-tTX5qlz6bPANR5mivFDZ_gp8KG0tWww",
+    authDomain: "train-scheduler-fe603.firebaseapp.com",
+    databaseURL: "https://train-scheduler-fe603.firebaseio.com",
+    projectId: "train-scheduler-fe603",
+    storageBucket: "train-scheduler-fe603.appspot.com",
+    messagingSenderId: "666823888874",
+    appId: "1:666823888874:web:7e26b8c0f48ddb203dac93"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ 
